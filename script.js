fb93655e5298bfc764e4ef988fea606a69db46c7
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-02-22 11:32:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+function findDOMLevel(elementId) {
+      var element = document.getElementById(elementId);
+      var domLevel = 1;
+
+      while (element && element !== document.documentElement) {
+        element = element.parentNode;
+        domLevel++;
+      }
+
+      alert("The level of the element is: " + domLevel);
+    }
+   
+    findDOMLevel("level");
